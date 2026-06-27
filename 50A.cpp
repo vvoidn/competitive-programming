19c2555aa12857ebbd7e35aa11fa10da6a6dda66
Suhas Prusty <suhasprusty@gmail.com>
2026-06-27 20:55:18 +0530

    codeforces:50A

---
+++ 50A.cpp
@@ -0,0 +1,13 @@
+
+//#dominos piling 
+#include <iostream>
+using namespace std;
+int main(){
+    int m,n;
+    cout<<"50A:domino piling"<<endl;
+    cout<<"enter number of rows(m) and columns(n):"<<endl;
+    cin>>m>>n;
+    int total=m*n;
+    cout<<total/2<<endl;
+    return 0;
+}
